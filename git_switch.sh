ff908954eaaf0b0a1501b3d840dc4ca2f21a12e4
bamjun <khy5116@naver.com>
2024-06-01 01:46:44 +0900

    Feat:check version

--- git_switch.sh
+++ git_switch.sh
@@ -1,8 +1,39 @@
 #!/bin/bash
 
-# ANSI escape codes for coloring
-# YELLOW_BG='\033[43m'
-# YELLOW_BG='\033[38;5;192m  \033[48;5;192m'
+##################### 버전관리 start
+
+version_index="0.0.0"
+
+
+# 버전 체크
+if [ "$1" = "check_version" ]; then
+    echo "$version_index"
+    exit
+fi
+
+github_repo_version=$(curl -s "https://bamjun.github.io/q/git_switch.sh" | bash -s -- check_version)
+
+if [ "$version_index" != "$github_repo_version" ]; then
+    echo "레포지토리 버전과 틀립니다."
+    echo "1 : 레포지토리 버전으로 덮어쓰기 (do ' temp' not '. temp')"
+    echo "2 : 그냥 종료"
+    echo "3 : 기존 스크립트 실행"
+    read choose_index
+    if [ "$choose_index" == "1" ]; then
+        script_path=$(realpath "$0")
+        curl -o $script_path "https://bamjun.github.io/q/git_switch.sh"
+        exit
+    elif [ "$choose_index" == "2" ]; then
+        exit
+    fi
+fi
+
+##################### 버전관리 end
+
+
+
+
+
 PURPLE_BG='\033[48;5;98m'
 SELECT_BG='\033[38;5;196m\033[48;5;192m'
 NO_COLOR='\033[0m'
