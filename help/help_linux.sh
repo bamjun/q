83fa7fc34c00dccc65aed3c9cc02de5b5a274cce
bamjun <khy5116@naver.com>
2024-06-20 14:12:17 +0900

    Feat:shells

--- help/help_linux.sh
+++ help/help_linux.sh
@@ -6,4 +6,10 @@ echo -e "\033[48;5;11m\033[38;5;21m◇  linux  ◇ ${COLOR_RESET}"
 
 echo -e "\033[48;5;228m\033[38;5;0m  - To check the disk usage of the top-level directories ${COLOR_RESET}"
 echo -e "\033[48;5;230m\033[38;5;0m    sudo du -h --max-depth=1 / ${COLOR_RESET}"
-
+echo -e "\033[48;5;228m\033[38;5;0m  - Check the Current Shell ${COLOR_RESET}"
+echo -e "\033[48;5;230m\033[38;5;0m    echo $SHELL ${COLOR_RESET}"
+echo -e "\033[48;5;230m\033[38;5;0m    echo grep [사용자] /etc/passwd ${COLOR_RESET}"
+echo -e "\033[48;5;228m\033[38;5;0m  - List Installed Shell ${COLOR_RESET}"
+echo -e "\033[48;5;230m\033[38;5;0m    cat /etc/shells ${COLOR_RESET}"
+echo -e "\033[48;5;228m\033[38;5;0m  - Change Shell ${COLOR_RESET}"
+echo -e "\033[48;5;230m\033[38;5;0m    chsh -s [쉘경로] ${COLOR_RESET}"
